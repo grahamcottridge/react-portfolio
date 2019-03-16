5bc3915c5b3e50f644ebe4d14bd688b86d2d2674
Graham Cottridge <codinggc@gmail.com>
2019-03-16 20:34:40 +0000

    Create header component

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
+class Header extends Component {
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#2f2f2f",
+          boxShadow: "none",
+          padding: "10px 0px"
+        }}
+      >
+        <Toolbar>
+          <div className="header-logo">
+            <div className="font-name header-logo-venue">Graham Cottridge</div>
+            <div className="header-logo-title">Web Developer</div>
+          </div>
+
+          <IconButton aria-label="Menu" color="inherit">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
