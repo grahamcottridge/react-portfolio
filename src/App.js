d1cd5bb1502d8c9272bc91ee0102dfeeeb8f1216
Graham Cottridge <codinggc@gmail.com>
2019-03-17 23:31:33 +0000

    Replace navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Header from "./components/header_footer/Header";
+import Card from "./components/portfolio/Card";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
+      <div className="App">
+        <div className="container">
+          <Header />
+        </div>
+        <Card />
       </div>
     );
   }
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -8,25 +8,65 @@ import IconButton from "@material-ui/core/IconButton";
 class Header extends Component {
   render() {
     return (
-      <AppBar
-        position="fixed"
-        style={{
-          backgroundColor: "#2f2f2f",
-          boxShadow: "none",
-          padding: "10px 0px"
-        }}
-      >
-        <Toolbar>
-          <div className="header-logo">
-            <div className="font-name header-logo-venue">Graham Cottridge</div>
-            <div className="header-logo-title">Web Developer</div>
-          </div>
+      <div className="navbar navbar-expand-md navbar-light bg-light">
+        <a href="#" className="navbar-brand">
+          Graham Cottridge
+        </a>
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarUnorderedList"
+          aria-controls="navbarUnorderedList"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarUnorderedList">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link active">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Portfolio
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-          <IconButton aria-label="Menu" color="inherit">
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      // <AppBar
+      //   position="fixed"
+      //   style={{
+      //     backgroundColor: "#2f2f2f",
+      //     boxShadow: "none",
+      //     padding: "10px 0px"
+      //   }}
+      // >
+      //   <Toolbar>
+      //     <div className="header-logo">
+      //       <div className="font-name header-logo-venue">Graham Cottridge</div>
+      //       <div className="header-logo-title">Web Developer</div>
+      //     </div>
+
+      //     <IconButton
+      //       aria-label="Menu"
+      //       color="inherit"
+      //       onClick={() => console.log("open")}
+      //     >
+      //       <MenuIcon />
+      //     </IconButton>
+      //   </Toolbar>
+      // </AppBar>
     );
   }
 }
